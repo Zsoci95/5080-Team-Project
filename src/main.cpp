b1130581b705e05b6c8a91fbc5d9f09e7fc2749e
Zsoci95 <el19zk@leeds.ac.uk>
2023-02-26 21:40:57 +0000

    Debug function added to use only one BNO

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "functions.h"
 
 #define BNO055_SAMPLERATE_DELAY_MS (10)
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVICE_UUID        "bd0f56c6-a403-4d3a-86ba-6fed11ce8473" //Randomly generated UUID
+#define CHARACTERISTIC_UUID "1fe90638-437c-490c-ad92-bda3b9423bab" 
 
 Adafruit_BNO055 bno_0 = Adafruit_BNO055(55, 0x28);
 Adafruit_BNO055 bno_1 = Adafruit_BNO055(55, 0x28);
@@ -11,6 +11,7 @@ BLEServer *m5_server;
 BLEService *m5_service;
 BLECharacteristic *m5_characteristic;
 String buffer = "";
+bool no_tca = false; //Used for debugging, if you don't have the TCA9548A chip, set this to true.
 
 int frequency_counter = 0;
 unsigned long start_time; 
@@ -35,35 +36,45 @@ void setup() {
   delay(1000);
   M5.Lcd.clear();
 
-  TCASelect(0);
-  if(!bno_0.begin())
-  {
-    Serial.print("Error: BNO0 not detected");
-    M5DisplayText("Error: BNO0 not detected", TFT_WIDTH / 2, TFT_HEIGHT / 2, 1, WHITE);
-    while(1);
+  if (no_tca) {
+    if (!bno_0.begin())
+    {
+      Serial.print("Error: BNO0 not detected");
+      M5DisplayText("Error: BNO0 not detected", TFT_WIDTH / 2, TFT_HEIGHT / 2, 1, WHITE);
+      while(1);
+      delay(1000);
+      bno_0.setExtCrystalUse(true);
+    }
   }
-  delay(1000);
-  bno_0.setExtCrystalUse(true);
+  else {
+    TCASelect(0);
+    if(!bno_0.begin()) {
+      Serial.print("Error: BNO0 not detected");
+      M5DisplayText("Error: BNO0 not detected", TFT_WIDTH / 2, TFT_HEIGHT / 2, 1, WHITE);
+      while(1);
+    }
+    delay(1000);
+    bno_0.setExtCrystalUse(true);
 
-  TCASelect(1);
-  if(!bno_1.begin())
-  {
-    Serial.print("Error: BNO1 not detected");
-    M5DisplayText("Error: BNO1 not detected", TFT_WIDTH / 2, TFT_HEIGHT / 2, 1, WHITE);
-    while(1);
-  }
-  delay(1000);
-  bno_1.setExtCrystalUse(true);
+    TCASelect(1);
+    if(!bno_1.begin()) {
+      Serial.print("Error: BNO1 not detected");
+      M5DisplayText("Error: BNO1 not detected", TFT_WIDTH / 2, TFT_HEIGHT / 2, 1, WHITE);
+      while(1);
+    }
+    delay(1000);
+    bno_1.setExtCrystalUse(true);
 
-  TCASelect(2);
-  if(!bno_2.begin())
-  {
-    Serial.print("Error: BNO2 not detected");
-    M5DisplayText("Error: BNO2 not detected", TFT_WIDTH / 2, TFT_HEIGHT / 2, 1, WHITE);
-    while(1);
+    TCASelect(2);
+    if(!bno_2.begin()) {
+      Serial.print("Error: BNO2 not detected");
+      M5DisplayText("Error: BNO2 not detected", TFT_WIDTH / 2, TFT_HEIGHT / 2, 1, WHITE);
+      while(1);
+    }
+    delay(1000);
+    bno_2.setExtCrystalUse(true);
   }
-  delay(1000);
-  bno_2.setExtCrystalUse(true);
+  
 
   BLEDevice::init("M5Stack-Server");
   m5_server = BLEDevice::createServer();
@@ -81,20 +92,29 @@ void setup() {
 
 void loop() {
   
-  TCASelect(0);
-  // read quaternion data
-  imu::Quaternion quat_0 = bno_0.getQuat();
-  // read accelerometer data
-  imu::Vector<3> accel_0 = bno_0.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  TCASelect(1);
-  imu::Quaternion quat_1 = bno_1.getQuat();
-  imu::Vector<3> accel_1 = bno_1.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  TCASelect(2);
-  imu::Quaternion quat_2 = bno_2.getQuat();
-  imu::Vector<3> accel_2 = bno_2.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  if (no_tca) {
+    imu::Quaternion quat_0 = bno_0.getQuat();
+    imu::Vector<3> accel_0 = bno_0.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    buffer = String((float)quat_0.w(), 4) + "," + String((float)quat_0.x(), 4) + "," + String((float)quat_0.y(), 4) + "," + String((float)quat_0.z(), 4) + "," + String((float)accel_0.x(), 4) + "," + String((float)accel_0.y(), 4) + "," + String((float)accel_0.z(), 4);
+
+  }
+  else {
+    TCASelect(0);
+    // read quaternion data
+    imu::Quaternion quat_0 = bno_0.getQuat();
+    // read accelerometer data
+    imu::Vector<3> accel_0 = bno_0.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    TCASelect(1);
+    imu::Quaternion quat_1 = bno_1.getQuat();
+    imu::Vector<3> accel_1 = bno_1.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    TCASelect(2);
+    imu::Quaternion quat_2 = bno_2.getQuat();
+    imu::Vector<3> accel_2 = bno_2.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    
+    buffer = String((float)quat_0.w(), 4) + "," + String((float)quat_0.x(), 4) + "," + String((float)quat_0.y(), 4) + "," + String((float)quat_0.z(), 4) + "," + String((float)quat_1.w(), 4) + "," + String((float)quat_1.x(), 4) + "," + String((float)quat_1.y(), 4) + "," + String((float)quat_1.z(), 4) + "," + String((float)quat_2.w(), 4) + "," + String((float)quat_2.x(), 4) + "," + String((float)quat_2.y(), 4) + "," + String((float)quat_2.z(), 4);
+    buffer += "," + String((float)accel_0.x(), 4) + "," + String((float)accel_0.y(), 4) + "," + String((float)accel_0.z(), 4) + "," + String((float)accel_1.x(), 4) + "," + String((float)accel_1.y(), 4) + "," + String((float)accel_1.z(), 4) + "," + String((float)accel_2.x(), 4) + "," + String((float)accel_2.y(), 4) + "," + String((float)accel_2.z(), 4);
+  }
   
-  buffer = String((float)quat_0.w(), 4) + "," + String((float)quat_0.x(), 4) + "," + String((float)quat_0.y(), 4) + "," + String((float)quat_0.z(), 4) + "," + String((float)quat_1.w(), 4) + "," + String((float)quat_1.x(), 4) + "," + String((float)quat_1.y(), 4) + "," + String((float)quat_1.z(), 4) + "," + String((float)quat_2.w(), 4) + "," + String((float)quat_2.x(), 4) + "," + String((float)quat_2.y(), 4) + "," + String((float)quat_2.z(), 4);
-  buffer += "," + String((float)accel_0.x(), 4) + "," + String((float)accel_0.y(), 4) + "," + String((float)accel_0.z(), 4) + "," + String((float)accel_1.x(), 4) + "," + String((float)accel_1.y(), 4) + "," + String((float)accel_1.z(), 4) + "," + String((float)accel_2.x(), 4) + "," + String((float)accel_2.y(), 4) + "," + String((float)accel_2.z(), 4);
   Serial.println(buffer);
 
   //send quaternion and accelerometer data via BLE
